c018529cc0b94fce34639f08cfbced5629e6e5b6
Kévin R <rifakev@gmail.com>
2023-05-18 18:17:42 +0200

    KR - Responsive Design

--- assets/app.js
+++ assets/app.js
@@ -63,13 +63,52 @@ navBtn.forEach(btn => {
     })
 })
 
+var navLink = document.querySelectorAll('.nav-link');
+navLink.forEach(btn => {
+    btn.addEventListener('click', function(){
+        htmlContent.classList.remove('nav-open');
+    })
+})
+
 
 // Service Tabs
 // -----------------------------------------------
-var serviceBtn = document.querySelectorAll('.service-btn');
-serviceBtn.forEach(btn => {
-    btn.addEventListener('click', function(){
-        console.log('cliqué');
-        console.log(btn.getAttribute('data-link'));
-    })
-})
+var tabs = require('tabs');
+var container = document.querySelector('.tab-container');
+
+if (container != null) {
+    tabs(container);
+}
+
+
+// Smooth Scroll
+// -----------------------------------------------
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+anchorLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    // Empêcher le comportement par défaut du lien
+    event.preventDefault();
+
+    // Récupérer l'ID de l'ancre cible
+    const targetId = link.getAttribute('href').substring(1);
+
+    // Faire défiler jusqu'à l'ancre cible sans changer l'URL
+    scrollSmoothly(targetId);
+  });
+});
+
+function scrollSmoothly(targetId) {
+  const targetElement = document.getElementById(targetId);
+  if (targetElement) {
+    // Calculer la position de l'ancre cible
+    const targetOffset = targetElement.offsetTop - 80;
+
+    // Effectuer le défilement en douceur vers l'ancre cible
+    const scrollOptions = {
+      top: targetOffset,
+      behavior: 'smooth'
+    };
+    window.scrollTo(scrollOptions);
+  }
+}
+
--- assets/smoothScroll.js
+++ assets/smoothScroll.js
@@ -7,7 +7,9 @@ export class ScrollWeb {
     }
 
     get init(){
-        const scrollbar = Scrollbar.init(document.querySelector('#content'), {
+        const container = document.querySelector('#content');
+        const header = document.querySelector('header');
+        const scrollbar = Scrollbar.init(container, {
             damping: (this.damping / 100),
             renderByPixels: true,
             continuousScrolling: true,
@@ -15,6 +17,29 @@ export class ScrollWeb {
             thumbMinSize: 15
             // alwaysShowTracks: true,
         });
+
+        // Détection du Scroll
+        scrollbar.addListener(function(){
+            const scrollY = scrollbar.offset.y;
+            if (scrollY > 50) {
+                document.querySelector('html').classList.add('onScroll');
+            } else {
+                document.querySelector('html').classList.remove('onScroll');
+            }
+        })
+
+        // Scroll au click d'une ancre
+        const navLinks = document.querySelectorAll('.nav-link');
+        navLinks.forEach(btn => {
+            btn.addEventListener('click', function(){
+                const target = btn.getAttribute('data-link');
+                const anchor = document.querySelector(target);
+                const offset = anchor.getBoundingClientRect().top - container.getBoundingClientRect().top;
+                scrollbar.scrollIntoView(anchor, { offset });
+                return false;
+            })
+        })
+
         return scrollbar;
     }
 }
