0bcaa0250fc1b55b581d1a2608c5664e05f06412
Kévin R <rifakev@gmail.com>
2023-06-06 23:48:04 +0200

    KR - Conformité RGPD

--- src/Form/NewsletterFormType.php
+++ src/Form/NewsletterFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -20,6 +21,12 @@ class NewsletterFormType extends AbstractType
                 ],
                 'attr' => [
                     'placeholder' => "Votre Adresse E-Mail",
+                ]
+            ])
+            ->add('rgpd_checkbox', CheckboxType::class, [
+                'label' => "En soumettant ce formulaire, j'accepte que mes données soient utilisées à des fins de marketing tout en sachant que je peux faire une demande de modification ou de suppression de mes données. Le formulaire est protégé par Google ReCaptcha",
+                'row_attr' => [
+                    'class' => 'form-input checkbox'
                 ]
             ])
             ->add('submit', SubmitType::class, [
